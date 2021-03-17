f325c2c7404f6cd2b5002ce43888b44d97ea60e8
José Pedro Belo <josepedroribeirobelo@usp.br>
2021-03-17 16:06:34 -0300

    updating init model

--- init_model.py
+++ init_model.py
@@ -1,37 +1,18 @@
 import torch
 
-from gmodel import Net
+from gmodel import DQN
 from pathlib import Path
 
-modelA = Net()
-modelB = Net()
+modelGray = DQN()
+modelDepth = DQN()
 
-save_modelA_gpu='results/ep0/modelA_cuda.net'
-save_modelB_gpu='results/ep0/modelB_cuda.net'
-save_tmodelA_gpu='results/ep0/tmodelA_cuda.net'
-save_tmodelB_gpu='results/ep0/tmodelB_cuda.net'
-save_modelA_cpu='results/ep0/modelA_cpu.net'
-save_modelB_cpu='results/ep0/modelB_cpu.net'
-save_tmodelA_cpu='results/ep0/tmodelA_cpu.net'
-save_tmodelB_cpu='results/ep0/tmodelB_cpu.net'
+save_modelGray='results/ep0/modelGray.net'
+save_modelDepth='results/ep0/modelDepth.net'
+save_tModelGray='results/ep0/tModelGray.net'
+save_tModelDepth='results/ep0/tModelDepth.net'
 Path('results/ep0').mkdir(parents=True, exist_ok=True)
-torch.save(modelA.to("cuda"),save_modelA_gpu)
-torch.save(modelB.to("cuda"),save_modelB_gpu)
-torch.save(modelA.to("cuda"),save_tmodelA_gpu)
-torch.save(modelB.to("cuda"),save_tmodelB_gpu)
+torch.save(modelGray,save_modelGray)
+torch.save(modelDepth,save_modelDepth)
+torch.save(modelGray,save_tModelGray)
+torch.save(modelDepth,save_tModelDepth)
 
-
-torch.save(modelA.to("cpu"),save_modelA_cpu)
-torch.save(modelB.to("cpu"),save_modelB_cpu)
-torch.save(modelA.to("cpu"),save_tmodelA_cpu)
-torch.save(modelB.to("cpu"),save_tmodelB_cpu)
-
-'''
-model=model:float()
-tmodel=tmodel:float()
-
-torch.save(model,save_modelA_cpu)
-torch.save(model,save_modelB_cpu)
-torch.save(model,save_tmodelA_cpu)
-torch.save(model,save_tmodelB_cpu	)
-'''
