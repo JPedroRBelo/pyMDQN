d1378a89b507d85a9a6cdc89ac780846388f484d
José Pedro Ribeiro Belo <josepedroribeirobelo@gmail.com>
2021-04-23 08:28:38 -0300

    training 24 eps with simv0.23

--- test_init.py
+++ test_init.py
@@ -1,18 +1,11 @@
-import torch
+import os
 
-from network import DQN
-from pathlib import Path
-
-modelGray = DQN()
-modelDepth = DQN()
-
-save_modelGray='results/ep0/modelGray.net'
-save_modelDepth='results/ep0/modelDepth.net'
-save_tModelGray='results/ep0/tModelGray.net'
-save_tModelDepth='results/ep0/tModelDepth.net'
-Path('results/ep0').mkdir(parents=True, exist_ok=True)
-torch.save(modelGray,save_modelGray)
-torch.save(modelDepth,save_modelDepth)
-torch.save(modelGray,save_tModelGray)
-torch.save(modelDepth,save_tModelDepth)
-
+os.system("rm -rf dataset/RGB/ep*")
+os.system("rm -rf dataset/RGB/ep*")
+os.system("rm -rf results/*")
+os.system("python makefiles.py")
+os.system("python init_model.py")
+#os.system("mkdir results/ep0")
+#os.system("cp -RT ep13/ results/ep0")
+os.system("python makefiles.py")
+os.system("python mdqn.py")
